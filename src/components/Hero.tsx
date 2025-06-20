976b854c6ec7baa8f96431598a2ee5709210d148
mateushilariodias <mateusilariodias@gmail.com>
2025-06-19 21:38:19 -0300

    style: start add information in hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,10 +1,10 @@
 'use client'
 
-function Hero(){
-    return(
+function Hero() {
+    return (
         <section className="bg-black text-white">
-
+            <h1>Descubra informações do profissional Mateus Dias</h1>
+            <p>Este site tem como objetivo hospedar os portfolios de Mateus Dias referente a cada uma das suas áreas de atuação.</p>
         </section>
     )
 }
-export default Hero;
