ea82f57f1f6bc17c2c5d0b134c5a242d82f83765
mateushilariodias <mateusilariodias@gmail.com>
2025-07-16 09:29:21 -0300

    chore(seo): install next-sitemap and create initial configuration file

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://portfolios-amber.vercel.app/', // 👉 troque pelo domínio real do seu projeto
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+};
